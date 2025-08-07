e94e2e244dea56d82069a7cb10af50db5b8787b2
Roy-hub-73 <roy@vonderlocht.de>
2025-08-07 17:46:50 +0200

    Update main.py

--- Client/main.py
+++ Client/main.py
@@ -2,13 +2,14 @@ import requests
 import json
 
 # === CONFIG CONSTANTS ===
-API_URL = "http://127.0.0.1:8000"  # Deine lokale API
-PDF_PATH = r"C:\Users\Roy von der Locht\Dropbox\Workspace\priv\Gesundheit\BEFUNDProfDirkHempel.pdf"          # Pfad zur PDF-Datei
-OUTPUT_PATH = r"C:\Users\Roy von der Locht\Dropbox\Workspace\DATA\SoftwareEntwicklung\SectionFinalProject\Files\extraktion.json"    # Pfad zur gespeicherten Antwort
+API_URL = "http://127.0.0.1:8000"           # Replace by your host address
+PDF_FILE_NAME = "your-filename-here"        # Just the filename, like "input_doctorsletter.pdf"
+PDF_PATH = r"your-path-and-file-here"       # Path to PDF file to read, like "c://Files/input_doctorsletter.pdf"
+OUTPUT_PATH = r"your-path-and-file-here"    # Path where to save the output to, like "c://Files/output.json"
 
 # === OPEN FILE AND CALL API ===
 with open(PDF_PATH, "rb") as pdf_file:
-    files = {"file": ("BEFUNDProfDirkHempel.pdf", pdf_file)}
+    files = {"file": (PDF_FILE_NAME, pdf_file)}
     response = requests.post(f"{API_URL}/extract_from_pdf", files=files)
 
 # === CHECK ANSWER AND SAVE JSON FILE ===
